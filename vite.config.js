6c3bfc163ec553cc45618ebfeefa10cbe12482a7
Frankeinstein <ronaldo2fael@gmail.com>
2026-05-18 13:16:34 +0300

    update_file

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,43 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import process from 'node:process';
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
-			},
-			'/images': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [react(), tailwindcss()],
+
+		define: {
+			'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL || ''),
+
+			'import.meta.env': env,
+			process: { env: env },
+		},
+
+		server: {
+			proxy: {
+				'/api': {
+					target: 'http://localhost:3000',
+					changeOrigin: true,
+				},
+				'/images': {
+					target: 'http://localhost:3000',
+					changeOrigin: true,
+				},
 			},
 		},
-	},
-	build: {
-		outDir: 'dist',
-		sourcemap: false,
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
+
+		build: {
+			outDir: 'dist',
+			sourcemap: false,
+			minify: 'terser',
+			terserOptions: {
+				compress: {
+					drop_console: true,
+				},
 			},
 		},
-	},
+	};
 });
